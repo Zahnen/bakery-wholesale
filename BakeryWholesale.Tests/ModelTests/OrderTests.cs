065eec9993b67717cd33ec9edecf3418a6c30f7e
Zahnen Garner <zahnen@gmail.com>
2020-12-18 09:39:58 -0800

    add code for second Order object test, fail test

--- BakeryWholesale.Tests/ModelTests/OrderTests.cs
+++ BakeryWholesale.Tests/ModelTests/OrderTests.cs
@@ -12,16 +12,17 @@ namespace BakeryWholesale.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-    Order newOrder = new Order();
+    Order newOrder = new Order("test");
     Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
     [TestMethod]
     public void OrderConstructor_ReturnsOrderTitle_String()
     {
       string orderName = "TestOrder";
+      string failName = "This is an example to compare to the string orderName for a failed test";
       Order newOrder = new Order(orderName);
-      string result = newOrder.orderName;
-      Assert.AreEqual(orderName, result);
+      string result = newOrder.OrderName;
+      Assert.AreEqual(failName, result);
     }
   }
 }
--- BakeryWholesale/Models/Order.cs
+++ BakeryWholesale/Models/Order.cs
@@ -5,6 +5,11 @@ namespace BakeryWholesale.Models
 {
   public class Order
   {
+    public string OrderName { get; set; }
 
+    public Order(string orderName)
+    {
+      OrderName = orderName;
+    }
   }
 }
