7223298cca00f9465d4b9276a1e717e616cb964b
Zahnen Garner <zahnen@gmail.com>
2020-12-18 09:12:36 -0800

    add first Order object test

---
+++ BakeryWholesale.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryWholesale.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace BakeryWholesale.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+    Order newOrder = new Order();
+    Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
