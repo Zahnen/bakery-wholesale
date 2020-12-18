599f563952f59570e11496e7e78d4557fe6f41f0
Zahnen Garner <zahnen@gmail.com>
2020-12-18 14:05:10 -0800

    add eighth Order object test

--- BakeryWholesale.Tests/ModelTests/OrderTests.cs
+++ BakeryWholesale.Tests/ModelTests/OrderTests.cs
@@ -92,5 +92,22 @@ namespace BakeryWholesale.Tests
       int result = newOrder.Id;
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order()
+    {
+      string orderName1 = "TestOrder1";
+      string orderDescription1 = "testExample1";
+      int orderPrice1 = 8;
+      string orderDate1 = "12/18/2020";
+      string orderName2 = "TestOrder2";
+      string orderDescription2 = "testExample2";
+      int orderPrice2 = 10;
+      string orderDate2 = "12/18/1992";
+      Order newOrder1 = new Order(orderName1, orderDescription1, orderPrice1, orderDate1);
+      Order newOrder2 = new Order(orderName2, orderDescription2, orderPrice2, orderDate2);
+
+      Order result = Order.Find(2);
+      Assert.AreEqual(newOrder2, result);
+    }
   }
 }
